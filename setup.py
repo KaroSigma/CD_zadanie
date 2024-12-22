a58f1c46ec554234a79476854129258bb54597da
KaroSigma <Karola9873@gmail.com>
2024-12-22 23:01:07 +0100

    fix: fix setup

---
+++ setup.py
@@ -0,0 +1,35 @@
+import os
+
+from setuptools import find_packages, setup
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+version = os.getenv(
+    "VERSION", "0.0.0"
+)  # Fallback to '0.0.0'version = os.getenv('PACKAGE_VERSION', '0.0.0')  # Fallback to '0.0.0'
+
+
+
+setup(
+    name="CD zadanie",
+    version=version,
+    author="Karolina Gluza",
+    author_email="karolina.gluza@edu.uekat.pl",
+    description="Attendance management system for university",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/KaroSigma/CD_zadanie",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": ["CD_zadanie=src:main"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+)
