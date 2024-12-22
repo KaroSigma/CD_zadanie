1f3346a2a1c594a8b9ea4a7889e400cf10347413
KaroSigma <Karola9873@gmail.com>
2024-12-22 23:38:29 +0100

    fix: setup.py name was wrong

--- setup.py
+++ setup.py
@@ -6,27 +6,27 @@ with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
 version = os.getenv(
-    "VERSION", "0.0.0"
+    "VERSION", "0.0.3"
 )  # Fallback to '0.0.0'version = os.getenv('PACKAGE_VERSION', '0.0.0')  # Fallback to '0.0.0'
 
 
 
 setup(
-    name="CD zadanie",
+    name="cd_zadanie",
     version=version,
     author="Karolina Gluza",
     author_email="karolina.gluza@edu.uekat.pl",
-    url="https://github.com/KaroSigma/CD_zadanie",
+    url="https://github.com/KaroSigma/cd_zadanie",
     packages=find_packages(),
     include_package_data=True,
     install_requires=requirements,
     entry_points={
-        "console_scripts": ["CD_zadanie=src:main"],
+        "console_scripts": ["cd_zadanie=src:main"],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.12",
+    python_requires=">=3.6",
 )
